0c06c7dea39d8e704ce66cff98c33b643fce3583
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-07 21:26:15 -0400

    feat: crear entidad Necesidad

---
+++ Microservicio-Necesidades/src/main/java/com/donaton/necesidadesservice/model/Necesidad.java
@@ -0,0 +1,20 @@
+package com.donaton.necesidades.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "necesidades")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Necesidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descripcion;
+    private int cantidadNecesaria;
+    private String ubicacion;
+}
