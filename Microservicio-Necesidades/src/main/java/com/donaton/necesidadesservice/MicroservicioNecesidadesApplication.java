360a342082e769402640755237b9e8f94459473c
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-07 21:21:52 -0400

    feat: inicializacion del microservicio de necesidades

---
+++ Microservicio-Necesidades/src/main/java/com/donaton/necesidadesservice/MicroservicioNecesidadesApplication.java
@@ -0,0 +1,13 @@
+package com.donaton.necesidadesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioNecesidadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioNecesidadesApplication.class, args);
+    }
+
+}
---
+++ Microservicio-Necesidades/src/test/java/com/donaton/necesidadesservice/MicroservicioNecesidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.necesidadesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioNecesidadesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
